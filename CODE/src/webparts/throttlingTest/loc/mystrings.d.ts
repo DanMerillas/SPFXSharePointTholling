cd3f77b4e9fee9ace51ea2ee07123571939d5956
Daniel Merillas <dmerillasg@minsait.com>
2023-03-16 19:57:19 +0100

    Creación de la solucion y script de poweshell

---
+++ CODE/src/webparts/throttlingTest/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IThrottlingTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ThrottlingTestWebPartStrings' {
+  const strings: IThrottlingTestWebPartStrings;
+  export = strings;
+}
---
+++ SCRIPTS/CreateListsExample.ps1
@@ -0,0 +1,80 @@
+#Config Variables
+$SiteURL = "https://Crescent.sharepoint.com"
+$ListNameNormal = "NormalList"
+$ListNameLarge = "LargeList"
+$ListNameVeryLarge = "VeryLargeList"
+
+$Template = "GenericList"
+ 
+
+ 
+Try {
+    #Connect to PnP Online
+    Connect-PnPOnline -Url $SiteURL -UseWebLogin
+ 
+    #sharepoint online create list pnp powershell
+    New-PnPList -Title $ListNameNormal -Template $Template -ErrorAction Stop
+    Write-host "List '$ListNameNormal' Created Successfully!" -f Green
+
+    New-PnPList -Title $ListNameLarge -Template $Template -ErrorAction Stop
+    Write-host "List '$ListNameLarge' Created Successfully!" -f Green
+
+    New-PnPList -Title $ListNameVeryLarge -Template $Template -ErrorAction Stop
+    Write-host "List '$ListNameVeryLarge' Created Successfully!" -f Green
+
+    #Insert Lists Data
+
+    #Create a New Batch
+    $Batch = New-PnPBatch
+ 
+    #Add List Items Normal list
+    For ($i = 1; $i -le 50; $i++) {
+        Add-PnPListItem -List $ListNameNormal -Values @{"Title" = "Value1" } -Batch $Batch
+    }
+
+    For ($i = 1; $i -le 20; $i++) {
+        Add-PnPListItem -List $ListNameNormal -Values @{"Title" = "Value2" } -Batch $Batch
+    }
+ 
+    #Send Batch to the server
+    Invoke-PnPBatch -Batch $Batch
+
+    Write-host "List '$ListNameNormal': Items Created Successfully!" -f Green
+
+    
+    #Add List Items Large list
+    For ($i = 1; $i -le 2500; $i++) {
+        Add-PnPListItem -List $ListNameLarge -Values @{"Title" = "Value1" } -Batch $Batch
+    }
+
+    For ($i = 1; $i -le 2500; $i++) {
+        Add-PnPListItem -List $ListNameLarge -Values @{"Title" = "Value2" } -Batch $Batch
+    }
+ 
+    #Send Batch to the server
+    Invoke-PnPBatch -Batch $Batch
+
+    Write-host "List '$ListNameLarge': Items Created Successfully!" -f Green
+
+
+
+    #Add List Items Large list
+    For ($i = 1; $i -le 6000; $i++) {
+        Add-PnPListItem -List $ListNameVeryLarge -Values @{"Title" = "Value1" } -Batch $Batch
+    }
+
+    For ($i = 1; $i -le 4000; $i++) {
+        Add-PnPListItem -List $ListNameVeryLarge -Values @{"Title" = "Value2" } -Batch $Batch
+    }
+ 
+    #Send Batch to the server
+    Invoke-PnPBatch -Batch $Batch
+    Write-host "List '$ListNameVeryLarge': Items Created Successfully!" -f Green
+
+
+
+}
+catch {
+    write-host "Error: $($_.Exception.Message)" -foregroundcolor Red
+}
+
